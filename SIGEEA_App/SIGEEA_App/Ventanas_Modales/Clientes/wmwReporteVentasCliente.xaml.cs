11096796049f5afeb801703f569a1dd7781747b7
Roberto2504 <robertoaf19@gmail.com>
2017-04-05 00:20:09 -0600

    me voy a cambiar de compu

---
+++ SIGEEA_App/SIGEEA_App/Ventanas_Modales/Clientes/wmwReporteVentasCliente.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using MahApps.Metro.Controls;
+using MahApps.Metro.Controls.Dialogs;
+using SIGEEA_BL;
+using SIGEEA_BO;
+using Microsoft.Reporting.WinForms;
+
+namespace SIGEEA_App.Ventanas_Modales.Clientes
+{
+    /// <summary>
+    /// Interaction logic for wmwReporteVentasCliente.xaml
+    /// </summary>
+    public partial class wmwReporteVentasCliente : MetroWindow
+    {
+        public wmwReporteVentasCliente()
+        {
+            InitializeComponent();
+        }
+    }
+}
